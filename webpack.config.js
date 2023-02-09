84c166f0dee51319a1043dd9ce991772d9b76bb3
Ivan Strilets <i.strilets@vk.team>
2023-02-09 14:31:32 +0300

    pug-loader => pug-plain-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,34 @@
-const path = require('path');
-const glob = require('glob');
+const path = require("path");
+const glob = require("glob");
 
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin')
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   entry: {
-    main: './src/js/index.js',
+    main: "./src/js/index.js",
   },
   output: {
-    filename: 'js/[name].js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: "js/[name].js",
+    path: path.resolve(__dirname, "dist"),
     clean: true,
   },
   resolve: {
     alias: {
-      'img': path.resolve(__dirname, 'src/images'),
-      'fonts': path.resolve(__dirname, 'src/fonts'),
-    }
+      img: path.resolve(__dirname, "src/images"),
+      fonts: path.resolve(__dirname, "src/fonts"),
+    },
   },
   plugins: [
-    ...glob.sync('./src/pug/pages/*.pug').map(htmlFile => {
+    ...glob.sync("./src/pug/pages/*.pug").map((htmlFile) => {
       return new HtmlWebpackPlugin({
-        filename: path.basename(htmlFile).replace(/\.pug/, '.html'),
+        filename: path.basename(htmlFile).replace(/\.pug/, ".html"),
         template: htmlFile,
       });
     }),
     new MiniCssExtractPlugin({
-      filename: 'css/[name].css',
+      filename: "css/[name].css",
     }),
     new CopyWebpackPlugin({
       patterns: [
@@ -42,39 +42,41 @@ module.exports = {
       {
         test: /\.js$/i,
         exclude: /node_modules/,
-        use: ['babel-loader'],
+        use: ["babel-loader"],
       },
       {
         test: /\.pug$/i,
-        loader: 'pug-loader',
-        options: {
-          pretty: true
-        }
+        use: ["raw-loader", "pug-plain-loader"],
       },
       {
         test: /\.s[ca]ss$/i,
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'sass-loader'],
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "postcss-loader",
+          "sass-loader",
+        ],
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         use: [
           {
-            loader: 'file-loader',
+            loader: "file-loader",
             options: {
-              name: '[name].[ext]',
-              publicPath: '../images',
-              outputPath: './images',
-            }
+              name: "[name].[ext]",
+              publicPath: "../images",
+              outputPath: "./images",
+            },
           },
-        ]
+        ],
       },
       {
         test: /\.(woff(2)?|ttf|eot)$/,
-        type: 'asset/resource',
+        type: "asset/resource",
         generator: {
-          filename: 'fonts/[name][ext]',
+          filename: "fonts/[name][ext]",
         },
       },
     ],
-  }
+  },
 };
